fc14231ec92f1f8037a84dc2f3693947088bae95
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-13 16:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+// script.js
 function firstChar(text) {
-  // your code here
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] !== ' ') {
+            return text[i];
+        }
+    }
+
+    // Return an empty string if the input string only contains spaces or is empty
+    return '';
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+const inputText = prompt("Enter text:");
+alert(firstChar(inputText));
